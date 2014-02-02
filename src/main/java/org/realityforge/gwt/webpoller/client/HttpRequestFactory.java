8422366453ffd0546129a1d0fa5c2ee4eb0f7ea4
Peter Donald <peter@realityforge.org>
2014-02-02 14:14:26 +1100

    Extract out parent class to make it easy to change poll target

--- src/main/java/org/realityforge/gwt/webpoller/client/HttpRequestFactory.java
+++ src/main/java/org/realityforge/gwt/webpoller/client/HttpRequestFactory.java
@@ -1,16 +1,13 @@
 package org.realityforge.gwt.webpoller.client;
 
-import com.google.gwt.http.client.Header;
 import com.google.gwt.http.client.RequestBuilder;
-import com.google.gwt.http.client.RequestCallback;
-import com.google.gwt.http.client.Response;
-import java.util.HashMap;
 import javax.annotation.Nonnull;
 
 /**
  * A request factory that uses the standard GWT HTTP module.
  */
 public final class HttpRequestFactory
+  extends AbstractHttpRequestFactory
   implements RequestFactory
 {
   private final RequestBuilder _requestBuilder;
@@ -20,65 +17,8 @@ public final class HttpRequestFactory
     _requestBuilder = requestBuilder;
   }
 
-  @Nonnull
-  @Override
-  public Request newRequest( @Nonnull final RequestContext requestContext )
-    throws Exception
+  protected final RequestBuilder getRequestBuilder()
   {
-    _requestBuilder.setCallback( new RequestCallback()
-    {
-      @Override
-      public void onResponseReceived( final com.google.gwt.http.client.Request request, final Response response )
-      {
-        final String data = response.getText();
-        if ( 0 == data.length() )
-        {
-          requestContext.onEmptyMessage();
-        }
-        else
-        {
-          final HashMap<String, String> context = new HashMap<>();
-          for ( final Header header : response.getHeaders() )
-          {
-            context.put( header.getName(), header.getValue() );
-          }
-          requestContext.onMessage( context, response.getText() );
-        }
-      }
-
-      @Override
-      public void onError( final com.google.gwt.http.client.Request request, final Throwable exception )
-      {
-        requestContext.onError( exception );
-      }
-    } );
-    return new HttpRequest( _requestBuilder.send() );
-  }
-
-  /**
-   * Adapter for HttpRequest to poller request.
-   */
-  private static class HttpRequest
-    implements Request
-  {
-    private com.google.gwt.http.client.Request _request;
-
-    private HttpRequest( final com.google.gwt.http.client.Request request )
-    {
-      _request = request;
-    }
-
-    @Override
-    public void cancel()
-    {
-      if ( null != _request )
-      {
-        if ( _request.isPending() )
-        {
-          _request.cancel();
-        }
-        _request = null;
-      }
-    }
+    return _requestBuilder;
   }
 }
